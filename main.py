dabfa84cf914d7be18d9ef0516bad2d2b9771e52
SvennieboyDev <Sven.Deijkers@gmail.com>
2024-10-27 11:37:29 +0100

    Basic Window met basic functionality
    27-10-2024

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
 #Gestart op 27 oktober 2024
+
+import kivy
+import kivymd
+from kivymd.app import MDApp
+from kivy.lang import Builder
+from kivymd.uix.screen import MDScreen
+from kivymd.uix.screenmanager import MDScreenManager
+
+class MainWindow(MDScreen):
+    pass
+
+class WindowManager(MDScreenManager):
+    pass
+
+class TimeSpentOn(MDApp):
+    def build(self):
+        self.title = "Time Spent On"
+
+        kv = Builder.load_file("timespenton.kv")
+        return kv
+    
+if __name__ == "__main__":
+    TimeSpentOn().run()
